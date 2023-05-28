8f86cfe3d41b490905d27125979bb1d4bf12a726
jihak <jooin2000@naver.com>
2023-05-28 22:54:27 +0900

    modify

--- cfg.py
+++ cfg.py
@@ -1,5 +1,6 @@
 import csv
 import sys
+from collections import deque
 
 # csv table을 dictionary로 바꿔주는 func
 def table_to_dic(filename):
@@ -33,11 +34,20 @@ if __name__ == "__main__":
     print(action_table[0]['vtype'])
     goto_table = table_to_dic('goto.csv')
     print(goto_table[0]['VDECL'])
-
+    table = table_to_dic('table.csv')
 
     with open(input_file,"r", encoding="UTF8") as f:
         data = f.read()
         print(data)
-    tokens = data.split() # list로 변환
+    input_data = deque(data.split()) # popleft하기 위해서 deque으로 변환
 
-    stack = [0]
+    input_data.append("$")
+    print(input_data)
+    stack = [0] # stack 선언
+
+    while(len(input_data) != 1):
+        x = input_data.popleft()
+        # 1) shift 연산인 경우
+        # 2) reduce 연산인 경우
+        table[stack[-1]][x]
+        
