b9812c9f9433c2ee9b2be2eb794e054240da1d68
DESKTOP-1IBJ60D\JIHAK <zhzhk17@gmail.com>
2023-05-31 00:44:08 +0900

    tree만 그리면 됨

--- cfg.py
+++ cfg.py
@@ -1,6 +1,6 @@
 import csv
 import sys
-from collections import deque
+from collections import deque, defaultdict
 
 # csv table을 dictionary로 바꿔주는 func
 def table_to_dic(filename):
@@ -17,6 +17,7 @@ def table_to_dic(filename):
                 temp_row_dict[key.strip()] = value.strip()
             table.append(temp_row_dict)
     return table
+
 def cfg_data(filename):
     data = []
     with open(filename, mode='r', encoding='UTF8') as file:
@@ -63,38 +64,56 @@ if __name__ == "__main__":
     input_data = deque(data.split()) # popleft하기 위해서 deque으로 변환
     input_data.append("$")
     stack = [0] # stack 선언
+    reduce = [] # reduce한 연산 넣어주기
+    try:
+        while(len(input_data) != 0):
+            print("Input: ",*input_data)
+            
+            x = input_data.popleft()
+            
+            command = table[stack[-1]][x]
+            print(f"command: {command}")
+            
+            # 1) shift 연산인 경우
+            if command[0] == "s":
+                stack.append(x)
+                stack.append(int(command[1:]))
+
+                print(f"shift: {stack}")
+                print()
+            # 2) reduce 연산인 경우
+            elif command[0] == "r":
+                input_data.appendleft(x) # 값을 다시 넣어줌
+                
+                l = (0 if cfg[int(command[1:])][1]==["''"] else len(cfg[int(command[1:])][1])) # 빈 문자열이면 0으로 해준다. 아니면 길이만큼
+                # 길이의 두배만큼 pop 해줘야함
+                temp_reduce = []
+                for i in range(l*2):
+                    
+                    stack.pop()
+                stack.append(cfg[int(command[1:])][0]) # reduce한거 append
+                reduce.append([cfg[int(command[1:])][0]])
+                reduce[-1].append(cfg[int(command[1:])][1])
+                print(f"reduce: {stack}")
+                print()
+
+                # goto 연산 실행
+                print("Input: ", *input_data)
+                print(f"command: {table[stack[-2]][stack[-1]]}")
+                stack.append(int(table[stack[-2]][stack[-1]]))
+                print(f"goto: {stack}")
+                print()
+    except:
+        print("reject")
+    print(stack)
     
-    while(len(input_data) != 1):
-        print(*input_data)
-        
-        x = input_data.popleft()
-        
-        
-        command = table[stack[-1]][x]
-        print(f"command: {command}")
-
-        # 1) shift 연산인 경우
-        if command[0] == "s":
-            stack.append(x)
-            stack.append(int(command[1:]))
-
-            print(f"shift: {stack}")
-            print()
-        # 2) reduce 연산인 경우
-        elif command[0] == "r":
-            input_data.appendleft(x) # 값을 다시 넣어줌
-
-            l = (0 if cfg[int(command[1:])][1]==["''"] else len(cfg[int(command[1:])][1])) # 빈 문자열이면 0으로 해준다. 아니면 길이만큼
-             # 길이의 두배만큼 pop 해줘야함
-            for i in range(l*2):
-                stack.pop()
-            stack.append(cfg[int(command[1:])][0]) # reduce한거 append
-            print(f"reduce: {stack}")
-            print()
-            # goto 연산 실행
-            print(*input_data)
-            print(f"command: {table[stack[-2]][stack[-1]]}")
-            stack.append(int(table[stack[-2]][stack[-1]]))
-            print(f"goto: {stack}")
-            print()
-        
+    # stack에 있는거 채워넣기
+    temp = []
+    for i in stack:
+        if not isinstance(i, int):
+            temp.append(i)
+    reduce.append(["CODE"])
+    reduce[-1].append(temp)
+    print(reduce)
+    # reduce = [['RHS', ['literal']], ['ASSIGN', ['id', 'assign', 'RHS']], ['VDECL', ['vtype', 'ASSIGN', 'semi']], ['ODECL', ["''"]], ...]]] 
+    
